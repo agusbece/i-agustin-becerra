655ae51efe148f51fb796ef18e116f12aa5978f5
Agustin Becerra <agustin.becerra@halopowered.com>
2023-10-26 18:11:13 -0300

    first change

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -69,18 +69,18 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Agustin Becerra.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
+        I'm a Nueve Helvecia based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
         at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
         registrar and site builder.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me training in <strong className="text-stone-100">calhistenic</strong>,
+        petting my <strong className="text-stone-100">dogs</strong>, or exploring this beautiful{' '}
+        <strong className="text-stone-100">world</strong>.
       </p>
     </>
   ),
